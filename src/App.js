5dc1ef44079b196a8b6dfc192aa952ec75f51462
TIAGO-DELL <tiagolbassani@gmail.com>
2021-03-29 10:55:35 -0300

    Add language to Congrats

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
 import Input from './Input';
+import LanguagePicker from './LanguagePicker';
+
 import { getSecretWord } from './actions';
+import languageContext from './contexts/languageContext';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
     case 'setSecretWord':
       return { ...prevState, setSecretWord: action.payload };
+    case 'setLanguage':
+      return { ...prevState, language: action.payload };
     default:
       throw new Error('Invalid Action type: ' + action.type);
   }
@@ -17,13 +22,17 @@ const reducer = (prevState, action) => {
 
 function App() {
   //const [secretWord, setSecretWord] = useState('');
-  const [state, dispatch] = useReducer(reducer, { secretWord: null });
+  const [state, dispatch] = useReducer(reducer, { secretWord: null, language: 'en' });
   // TODO: get props from shared state
   const success = false;
   const guessedWords = [];
 
   const setSecretWord = (secretWord) => {
     dispatch({ type: 'setSecretWord', payload: secretWord });
+  };
+
+  const setLanguage = (language) => {
+    dispatch({ type: 'setLanguage', payload: language });
   };
 
   useEffect(() => {
@@ -44,9 +53,12 @@ function App() {
   return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
-      <Congrats success={success} />
-      <Input success={success} secretWord={state.secretWord} />
-      <GuessedWords guessedWords={guessedWords} />
+      <languageContext.Provider value={state.language}>
+        <LanguagePicker setLanguage={setLanguage} />
+        <Congrats success={success} />
+        <Input success={success} secretWord={state.secretWord} />
+        <GuessedWords guessedWords={guessedWords} />
+      </languageContext.Provider>
     </div>
   );
 }
